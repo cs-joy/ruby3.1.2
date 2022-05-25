3cc59031f3b5adc284f5efa48982f6765f8c3ab4
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-05-26 01:13:30 +0600

    Update first.rb

--- first.rb
+++ first.rb
@@ -1,1 +1,16 @@
-puts "Hello World!"
+puts 'Hello World!'
+
+puts 'escae using "\\"'
+puts 'that\'s right, what you already say to me!'
+
+def hi
+    puts "Hello Ruby!"
+end
+
+puts hi()
+
+def hey(name)
+    puts "Hello #{name}!"
+end
+
+puts hey("sj")
