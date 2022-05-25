eee5a718af5c4657aa526146639a0872d5260d01
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-05-26 01:33:56 +0600

    Update first.rb

--- first.rb
+++ first.rb
@@ -3,6 +3,7 @@ puts 'Hello World!'
 puts 'escae using "\\"'
 puts 'that\'s right, what you already say to me!'
 
+# Repetitive Lives of a Method
 def hi
     puts "Hello Ruby!"
 end
@@ -14,3 +15,15 @@ def hey(name)
 end
 
 puts hey("sj")
+
+puts hey "cooler"
+
+# Holding Spots in a String
+def hi(name = 'World')
+    puts "Hello #{name.capitalize}!"
+end
+
+puts hi 'chris'
+puts hi
+puts hi("willams")
+puts hi('willams')
