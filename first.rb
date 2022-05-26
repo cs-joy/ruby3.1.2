3bb1e417581932e3c3034098e7158474dde368ce
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-05-26 10:19:11 +0600

    Update first.rb

--- first.rb
+++ first.rb
@@ -63,6 +63,9 @@ puts "------------------"
 var = Greeter.instance_methods(false)
 puts var
 puts "-------------------"
+
+# let’s see which methods our greeter object responds to:
+
 var = obj.respond_to?("name") # false - because it doesn’t know `name`
 puts var
 
@@ -71,3 +74,34 @@ puts var
 
 var = obj.respond_to?("to_s") # true by default- `to_s` meaning convert something to a string, a method that’s defined by default for every object
 puts var
+
+puts "-------------------"
+# Altering Classes—It’s Never Too Late
+class Greeter
+    attr_accessor :name # Using attr_accessor defined two new methods for us, `name` to get the value, and `name=` to set it.
+end
+
+obj2 = Greeter.new("js")
+obj2.say_hi
+obj2.say_bye
+
+# object responds to
+var = obj2.respond_to?("name")
+puts var
+
+var = obj2.respond_to?("name=")
+puts var
+
+var = obj2.respond_to?("hi")
+puts var
+
+puts "--------------"
+obj2.say_hi
+var = obj2.name
+puts var
+
+puts "--------------"
+obj2.name="sz"
+
+obj2.say_hi
+obj2.say_bye
